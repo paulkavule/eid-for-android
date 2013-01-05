5af20b5b678dc7a5c9f34e78d2691d81efcd0f2d
bbroucka@gmail.com <bbroucka@gmail.com@5c60ebee-4b43-3e38-14e1-b0d5e019359a>
2013-01-05 15:07:36 +0000

    Some UI experience improvements

--- src/net/egelke/android/eid/CertificateFragment.java
+++ src/net/egelke/android/eid/CertificateFragment.java
@@ -1,16 +1,25 @@
 package net.egelke.android.eid;
 
 import android.app.Fragment;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 public class CertificateFragment extends Fragment {
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.certificates, container, false);
+		View v = inflater.inflate(R.layout.certificates, container, false);
+		
+		if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+			LinearLayout layout = (LinearLayout) v.findViewById(R.id.certLayout);
+			layout.setOrientation(LinearLayout.VERTICAL);
+		}
+		
+		return v;
 	}
 
 	public void updateCertificates() {

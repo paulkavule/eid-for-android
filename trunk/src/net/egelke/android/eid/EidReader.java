0cb6c0cff45264bcd22b38a857e208d7d3ceec21
bbroucka@gmail.com <bbroucka@gmail.com@5c60ebee-4b43-3e38-14e1-b0d5e019359a>
2013-01-05 18:33:06 +0000

    Keep data on configuration change

--- trunk/src/net/egelke/android/eid/EidReader.java
+++ trunk/src/net/egelke/android/eid/EidReader.java
@@ -8,7 +8,6 @@ import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
